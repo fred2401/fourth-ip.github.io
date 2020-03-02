eb2a78f4d783309d5d6df45500222c1b198f2185
frederick asacha <fredasacha2@gmail.com>
2020-03-02 10:49:19 +0300

    made a delivery form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 
-function Contact(first, last, ) {
+
+
+function Contact(first, last, estate, house) {
     this.firstName = first;
     this.lastName = last;
+    this.onlyEstate = estate;
+    this.onlyHouse = house;
   }
   
   
@@ -11,12 +15,25 @@ function Contact(first, last, ) {
   
       var inputtedFirstName = $("input#new-first-name").val();
       var inputtedLastName = $("input#new-last-name").val();
+      var inputtedEstate = $("input#new-estate").val();
+      var inputtedHouse = $("input#new-house").val();
   
-      var newContact = new Contact(inputtedFirstName, inputtedLastName);
+      var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedEstate, inputtedHouse);
+
+      Contact.prototype.name=function(){
+          return inputtedFirstName + " " + inputtedLastName;
+      }
+
+      Contact.prototype.address=function(){
+        return inputtedEstate+ " " + inputtedHouse;
+      }
   
-      $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+      $("ul#contacts").append("<li><span class='contact'>" + newContact.name() + " " + newContact.address() + "</span></li>");
   
       $("input#new-first-name").val("");
       $("input#new-last-name").val("");
+      $("input#new-estate").val("");
+      $("input#new-house").val("");
+
     });
   }); 
