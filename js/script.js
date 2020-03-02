8f9c2b70d080ebe0cc8d39598db8c7953882777e
frederick asacha <fredasacha2@gmail.com>
2020-03-02 09:16:01 +0300

    made a basic form to trace my steps

--- js/script.js
+++ js/script.js
@@ -1,37 +1,22 @@
-function Contact(first, last) {
+
+function Contact(first, last, ) {
     this.firstName = first;
     this.lastName = last;
-    this.addresses = [];
-}
-function Address(house, estate) {
-    this.house = house;
-    this.estate = estate;
-}
-$(document).ready(function() {
-    $("form#contact").submit(function(event) {
+  }
+  
+  
+  $(document).ready(function() {
+    $("form#new-contact").submit(function(event) {
       event.preventDefault();
   
-      var inputtedFirstName = $("input#first-name").val();
-      var inputtedLastName = $("input#last-name").val();
-      var inputtedHouse = $("input#house").val();
-      var inputtedEstate = $("input#estate").val();
+      var inputtedFirstName = $("input#new-first-name").val();
+      var inputtedLastName = $("input#new-last-name").val();
   
       var newContact = new Contact(inputtedFirstName, inputtedLastName);
-      var newAddress= new Address(inputtedEstate, inputtedHouse)
-
-      Contact.prototype.fullName = function() {
-        return this.firstName + " " + this.lastName;
-      }
-      Address.prototype.fullAddress = function() {
-        return this.estate + " " + this.house;
-      }
   
-      $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName()<br>newAddress.fullAddress() + "</span></li>");
+      $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
   
-      $("input#first-name").val("");
-      $("input#last-name").val("");
-      $("input#house").val("");
-      $("input#estate").val("");
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
     });
-}); 
-
+  }); 
