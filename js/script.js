a126af3246b448324abb14407bf8e4bae8f787ad
frederick asacha <fredasacha2@gmail.com>
2020-03-03 09:01:57 +0300

    added an script for the selection of pizza

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-
+$(document).ready(function() {
+    $(".delivery").click(function() {
+      $(".delivery").toggle();
+      $(".delivery-show").toggle();
+    });
+    $(".delivery-show").click(function() {
+        $(".delivery-show").toggle();
+        $(".delivery").toggle();
+      });
+  });
 
 
 function Contact(first, last, estate, house) {
@@ -39,3 +48,161 @@ function Contact(first, last, estate, house) {
 
     });
   }); 
+
+function data(){
+        //hawaiian//
+        //small//
+    if ((hawaiian.checked==true) & (small.checked==true) & (thin.checked==true)){
+        alert("you've ordered a small hawaiian with thin crust");
+        alert("the total is 3$")
+    }
+    
+    else if ((hawaiian.checked==true) & (small.checked==true) & (thick.checked==true)){
+        alert("you've ordered a small hawaiian with thick crust");
+        alert("the total is 3$")
+    }
+        //medium//
+    else if ((hawaiian.checked==true) & (medium.checked==true) & (thin.checked==true)){
+        alert("you've ordered a medium hawaiian with thin crust");
+        alert("the total is 5$")
+    }
+    else if ((hawaiian.checked==true) & (medium.checked==true) & (thick.checked==true)){
+        alert("you've ordered a medium hawaiian with thick crust");
+        alert("the total is 5$")
+    }
+        //large//
+    else if ((hawaiian.checked==true) & (large.checked==true) & (thin.checked==true)){
+        alert("you've ordered a large hawaiian with thin crust");
+        alert("the total is 7$")
+    }
+    else if ((hawaiian.checked==true) & (large.checked==true) & (thick.checked==true)){
+        alert("you've ordered a large hawaiian with thick crust");
+        alert("the total is 7$")
+    }
+
+        //regina//
+        //small//
+    if ((regina.checked==true) & (small.checked==true) & (thin.checked==true)){
+        alert("you've ordered a small regina with thin crust");
+        alert("the total is 3.5$")
+    }
+    
+    else if ((regina.checked==true) & (small.checked==true) & (thick.checked==true)){
+        alert("you've ordered a small regina with thick crust");
+        alert("the total is 3.5$")
+    }
+        //medium//
+    else if ((regina.checked==true) & (medium.checked==true) & (thin.checked==true)){
+        alert("you've ordered a medium regina with thin crust");
+        alert("the total is 5.5$")
+    }
+    else if ((regina.checked==true) & (medium.checked==true) & (thick.checked==true)){
+        alert("you've ordered a medium regina with thick crust");
+        alert("the total is 5.5$")
+    }
+        //large//
+    else if ((regina.checked==true) & (large.checked==true) & (thin.checked==true)){
+        alert("you've ordered a large regina with thin crust");
+        alert("the total is 7.5$")
+    }
+    else if ((regina.checked==true) & (large.checked==true) & (thick.checked==true)){
+        alert("you've ordered a large regina with thick crust");
+        alert("the total is 7.5$")
+    }
+    
+        //boerewors//
+        //small//
+    if ((boerewors.checked==true) & (small.checked==true) & (thin.checked==true)){
+        alert("you've ordered a small boerewors with thin crust");
+        alert("the total is 2.5$")
+    }
+    
+    else if ((boerewors.checked==true) & (small.checked==true) & (thick.checked==true)){
+        alert("you've ordered a small boerewors with thick crust");
+        alert("the total is 2.5$")
+    }
+        //medium//
+    else if ((boerewors.checked==true) & (medium.checked==true) & (thin.checked==true)){
+        alert("you've ordered a medium boerewors with thin crust");
+        alert("the total is 4$")
+    }
+    else if ((boerewors.checked==true) & (medium.checked==true) & (thick.checked==true)){
+        alert("you've ordered a medium boerewors with thick crust");
+        alert("the total is 4$")
+    }
+        //large//
+    else if ((boerewors.checked==true) & (large.checked==true) & (thin.checked==true)){
+        alert("you've ordered a large boerewors with thin crust");
+        alert("the total is 6.5$")
+    }
+    else if ((boerewors.checked==true) & (large.checked==true) & (thick.checked==true)){
+        alert("you've ordered a large boerewors with thick crust");
+        alert("the total is 6.5$")
+    }
+
+        //BBQ-steak//
+        //small//
+    if ((BBQsteak.checked==true) & (small.checked==true) & (thin.checked==true)){
+        alert("you've ordered a small BBQ steak with thin crust");
+        alert("the total is 2.5$")
+    }
+    else if ((BBQsteak.checked==true) & (small.checked==true) & (thick.checked==true)){
+        alert("you've ordered a small BBQ steak with thick crust");
+        alert("the total is 2.5$")
+    }
+        //medium//
+    else if ((BBQsteak.checked==true) & (medium.checked==true) & (thin.checked==true)){
+        alert("you've ordered a medium BBQ steak with thin crust");
+        alert("the total is 2.5$")
+    }
+    else if ((BBQsteak.checked==true) & (medium.checked==true) & (thick.checked==true)){
+        alert("you've ordered a medium BBQ steak with thick crust");
+        alert("the total is 2.5$")
+    }
+        //large//
+    else if ((BBQsteak.checked==true) & (large.checked==true) & (thin.checked==true)){
+        alert("you've ordered a large BBQ steak with thin crust");
+        alert("the total is 2.5$")
+    }
+    else if ((BBQsteak.checked==true) & (large.checked==true) & (thick.checked==true)){
+        alert("you've ordered a large BBQ steak with thick crust");
+        alert("the total is 2.5$")
+    }
+
+        //veg-feast//
+        //small//
+    if ((vegfeast.checked==true) & (small.checked==true) & (thin.checked==true)){
+        alert("you've ordered a small veg feast with thin crust");
+        alert("the total is 2.5$")
+    }
+    
+    else if ((vegfeast.checked==true) & (small.checked==true) & (thick.checked==true)){
+        alert("you've ordered a small veg feast with thick crust");
+        alert("the total is 2.5$")
+    }
+        //medium//
+    else if ((vegfeast.checked==true) & (medium.checked==true) & (thin.checked==true)){
+        alert("you've ordered a medium veg feast with thin crust");
+        alert("the total is 2.5$")
+    }
+    else if ((vegfeast.checked==true) & (medium.checked==true) & (thick.checked==true)){
+        alert("you've ordered a medium veg feast with thick crust");
+        alert("the total is 2.5$")
+    }
+        //large//
+    else if ((vegfeast.checked==true) & (large.checked==true) & (thin.checked==true)){
+        alert("you've ordered a large veg feast with thin crust");
+        alert("the total is 2.5$")
+    }
+    else if ((vegfeast.checked==true) & (large.checked==true) & (thick.checked==true)){
+        alert("you've ordered a large veg feast with thick crust");
+        alert("the total is 2.5$")
+    }
+
+
+
+    // // if not picked//
+    // else{
+    //     alert("place an order")
+    // }
+}
